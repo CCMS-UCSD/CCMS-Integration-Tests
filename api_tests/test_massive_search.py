ec8b232aa28ef3abfbccd47ee0b813f60ac70d4f
jjcarver <jjcarver@ucsd.edu>
2020-02-27 13:38:43 -0800

    Increased timeout to 20 seconds for potentially slower MassIVE search queries.

--- api_tests/test_massive_search.py
+++ api_tests/test_massive_search.py
@@ -6,10 +6,10 @@ def test_search_loadtime():
     requests.get(url, timeout=10).raise_for_status()
 
     url = "http://massive.ucsd.edu/ProteoSAFe/QueryPROXI?task=N%2FA&file=&pageSize=30&offset=0&query=%2523%257B%2522searched_button%2522%253A%2522peptides%2522%257D&query_type=peptide&_=1489704532487"
-    requests.get(url, timeout=10).raise_for_status()
+    requests.get(url, timeout=20).raise_for_status()
 
     url = "http://massive.ucsd.edu/ProteoSAFe/QueryPROXI?task=N%2FA&file=&pageSize=30&offset=0&query=%2523%257B%2522searched_button%2522%253A%2522variants%2522%257D&query_type=variant&_=1489704532488"
-    requests.get(url, timeout=10).raise_for_status()
+    requests.get(url, timeout=20).raise_for_status()
 
     url = "http://massive.ucsd.edu/ProteoSAFe/QueryPROXI?task=N%2FA&file=&pageSize=30&offset=0&query=%2523%257B%2522searched_button%2522%253A%2522ptms%2522%257D&query_type=modification&_=1489704532489"
     requests.get(url, timeout=10).raise_for_status()
@@ -21,5 +21,5 @@ def test_search_loadtime():
     requests.get(url, timeout=10).raise_for_status()
 
     url = "http://massive.ucsd.edu/ProteoSAFe/QueryPROXI?task=N%2FA&file=&pageSize=30&offset=0&query=%2523%257B%2522searched_button%2522%253A%2522peptides%2522%252C%2522table_sort_history%2522%253A%2522variant_sequence_dsc%253Bsequence_dsc%2522%257D&query_type=peptide&_=1489704532502"
-    requests.get(url, timeout=10).raise_for_status()
+    requests.get(url, timeout=20).raise_for_status()
 
