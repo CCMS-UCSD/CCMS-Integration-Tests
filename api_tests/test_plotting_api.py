21f62c51e229e70ebe351fdaebfa8a2188d2cd6e
Mingxun Wang <mwang87@gmail.com>
2020-04-28 13:17:46 -0700

    forcing regeneration

--- api_tests/test_plotting_api.py
+++ api_tests/test_plotting_api.py
@@ -1,7 +1,7 @@
 import requests
 
 def test_internal_plot_api():
-    url = "https://ccms-internal.ucsd.edu/ProteoSAFe/DownloadResultFile?invoke=annotatedSpectrumImageText&task=8c2674bb0d40482fb813c83580fb0766&block=0&file=FILE-%3Ef.test%2Fdemo%2FLens%2Fspectra%2F93yo_sol_31_32.mgf&scan=-1&index=267&peptide=*..*&trim=true"
+    url = "https://ccms-internal.ucsd.edu/ProteoSAFe/DownloadResultFile?invoke=annotatedSpectrumImageText&task=8c2674bb0d40482fb813c83580fb0766&block=0&file=FILE-%3Ef.test%2Fdemo%2FLens%2Fspectra%2F93yo_sol_31_32.mgf&scan=-1&index=267&peptide=*..*&trim=true&force=true"
     r = requests.get(url, verify=False)
     r.raise_for_status()
 
