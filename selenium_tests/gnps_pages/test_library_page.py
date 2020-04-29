6eaf99346dcebc8d598b92d1cd7d57f17234841b
Mingxun Wang <mwang87@gmail.com>
2020-04-29 15:16:20 -0700

    updating library rendering delay

--- selenium_tests/gnps_pages/test_library_page.py
+++ selenium_tests/gnps_pages/test_library_page.py
@@ -25,14 +25,14 @@ class TestInterfaceready(unittest.TestCase):
     def test_library_page_table(self):
         self.driver.get("{}/ProteoSAFe/libraries.jsp?test=true".format(self.base_url))
         
-        time.sleep(5)
+        time.sleep(15)
         
         elements = self.driver.find_element_by_id("other_library_spectra.library_membership_input")
 
     def test_library_page_table2(self):
         self.driver.get("{}/ProteoSAFe/gnpslibrary.jsp?library=GNPS-LIBRARY&test=true".format(self.base_url))
         
-        time.sleep(5)
+        time.sleep(15)
         
         elements = self.driver.find_element_by_id("main.spectrum_id_input")
 
