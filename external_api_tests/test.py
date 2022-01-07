0556c0ba89a86b1c66c3dac2d76497bcf264d467
Mingxun Wang <mwang87@gmail.com>
2022-01-07 09:14:44 -0800

    updating

--- external_api_tests/test.py
+++ external_api_tests/test.py
@@ -8,14 +8,11 @@ import requests
 
 import os
 
+def test_ms2lda_motifdb():
+    server_url = 'https://ms2lda.org/motifdb/'
 
-def test_ms2lda():
-    url = "http://ms2lda.org/"
-    r = requests.get(url)
-    r.raise_for_status()
-
-    server_url = 'http://ms2lda.org/motifdb/'
-    motifset_dict = requests.get(server_url+'list_motifsets/', timeout=900).json()
+    print('Grabbing the latest Motifs from MS2LDA')
+    motifset_dict = requests.get(server_url + 'list_motifsets/').json()
 
     db_list = []
     db_list.append(2)
@@ -24,12 +21,17 @@ def test_ms2lda():
     db_list.append(3)
     db_list.append(5)
     db_list.append(6)
+    db_list.append(16)
 
-    client = requests.session()
-    token_output = client.get(server_url + 'initialise_api/', timeout=900).json()
-    token = token_output['token']
-    data = {'csrfmiddlewaretoken':token}
+    data = {}
     data['motifset_id_list'] = db_list
     data['filter'] = 'True'
 
-    output = client.post(server_url + 'get_motifset/',data = data, timeout=900).json()
+    print('Getting motifsets')
+    response = requests.post(server_url + 'get_motifset/', data=data)
+    json_output = response.json()
+
+    assert len(motifset_dict) > 0
+    assert response.status_code == 200
+    assert len(json_output['motifs']) > 0
+    assert len(json_output['metadata']) > 0
