682df803161fbc8b838aec400f094f90a716e7be
Jeremy Carver <jjcarver@ucsd.edu>
2024-10-04 08:35:44 -0700

    Tweaked formatting of test failure notification email.

--- status_notification/test_notify.py
+++ status_notification/test_notify.py
@@ -10,12 +10,12 @@ TRIGGERING_WORKFLOW_NAME = os.getenv("TRIGGERING_WORKFLOW_NAME")
 TRIGGERING_WORKFLOW_ID = os.getenv("TRIGGERING_WORKFLOW_ID")
 NOTIFICATION_EMAIL = os.getenv("NOTIFICATION_EMAIL")
 NOTIFICATION_PASSWORD = os.getenv("NOTIFICATION_PASSWORD")
-TIMESTAMP_FORMAT = "%A %d/%m/%Y"
+NOTIFICATION_RECIPIENT = "jjcarver@ucsd.edu,bandeira@ucsd.edu"
+TIMESTAMP_FORMAT = "%I:%M:%S %p %A %d/%m/%Y"
 GITHUB_TEST_URL_BASE = "https://github.com/CCMS-UCSD/CCMS-Integration-Tests/actions/runs/"
-RECIPIENT = "jjcarver@ucsd.edu,bandeira@ucsd.edu"
 
 def test_notify_on_workflow_failure():
-    subject = "[CCMS] GitHub CCMS-Integration-Tests Workflow Failure: " + TRIGGERING_WORKFLOW_NAME + " (" + TRIGGERING_WORKFLOW_ID + ")"
+    subject = "GitHub CCMS-Integration-Tests Workflow Failure: " + TRIGGERING_WORKFLOW_NAME
     now = datetime.datetime.now().strftime(TIMESTAMP_FORMAT)
     test_link = GITHUB_TEST_URL_BASE + TRIGGERING_WORKFLOW_ID
     email_body = f"""
@@ -25,9 +25,9 @@ Test workflow {TRIGGERING_WORKFLOW_NAME} (ID {TRIGGERING_WORKFLOW_ID}) failed: {
 """
     message = email.message.EmailMessage()
     message.set_content(email_body, subtype="html")
-    message["Subject"] = subject
+    message["Subject"] = "[CCMS] " + subject
     message["From"] = NOTIFICATION_EMAIL
-    message["To"] = RECIPIENT
+    message["To"] = NOTIFICATION_RECIPIENT
     smtp = smtplib.SMTP("smtp.gmail.com", 587)
     smtp.starttls()
     smtp.login(NOTIFICATION_EMAIL, NOTIFICATION_PASSWORD)
