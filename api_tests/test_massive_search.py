c7d9c0302aa6104d1edba963f3a156cc75cc13c1
Jeremy Carver <jjcarver@ucsd.edu>
2024-10-04 08:42:04 -0700

    Increased test timeouts
    More test timeout increases to accommodate occasional server delays.

--- api_tests/test_massive_search.py
+++ api_tests/test_massive_search.py
@@ -2,7 +2,7 @@ import requests
 
 def test_search_loadtime():
     url = "http://massive.ucsd.edu/ProteoSAFe/QueryPROXI?task=N%2FA&file=&pageSize=30&offset=0&query=%2523%257B%2522searched_button%2522%253A%2522psms%2522%257D&query_type=psm&_=1489704532486"
-    response = requests.get(url, timeout=10)
+    response = requests.get(url, timeout=60)
     content = response.json()
     assert(content["source"] == "PROXI")
     assert(len(content["row_data"]) == 30)
@@ -17,7 +17,7 @@ def test_search_loadtime():
     response.raise_for_status()
 
     url = "http://massive.ucsd.edu/ProteoSAFe/QueryPROXI?task=N%2FA&file=&pageSize=30&offset=0&query=%2523%257B%2522searched_button%2522%253A%2522peptides%2522%257D&query_type=peptide&_=1489704532487"
-    response = requests.get(url, timeout=20)
+    response = requests.get(url, timeout=60)
     content = response.json()
     assert(content["source"] == "PROXI")
     assert(len(content["row_data"]) == 30)
@@ -25,7 +25,7 @@ def test_search_loadtime():
     response.raise_for_status()
 
     url = "http://massive.ucsd.edu/ProteoSAFe/QueryPROXI?task=N%2FA&file=&pageSize=30&offset=0&query=%2523%257B%2522searched_button%2522%253A%2522variants%2522%257D&query_type=variant&_=1489704532488"
-    response = requests.get(url, timeout=30)
+    response = requests.get(url, timeout=60)
     content = response.json()
     assert(content["source"] == "PROXI")
     assert(len(content["row_data"]) == 30)
@@ -35,7 +35,7 @@ def test_search_loadtime():
     response.raise_for_status()
 
     url = "http://massive.ucsd.edu/ProteoSAFe/QueryPROXI?task=N%2FA&file=&pageSize=30&offset=0&query=%2523%257B%2522searched_button%2522%253A%2522proteins%2522%257D&query_type=protein&_=1489704532490"
-    response = requests.get(url, timeout=10)
+    response = requests.get(url, timeout=60)
     content = response.json()
     assert(content["source"] == "PROXI")
     assert(len(content["row_data"]) == 30)
@@ -43,7 +43,7 @@ def test_search_loadtime():
     response.raise_for_status()
 
     url = "http://massive.ucsd.edu/ProteoSAFe/QueryPROXI?task=N%2FA&file=&pageSize=30&offset=0&query=%2523%257B%2522searched_button%2522%253A%2522ptms%2522%257D&query_type=modification&_=1489704532489"
-    response = requests.get(url, timeout=10)
+    response = requests.get(url, timeout=60)
     content = response.json()
     assert(content["source"] == "PROXI")
     assert(len(content["row_data"]) == 30)
@@ -52,7 +52,7 @@ def test_search_loadtime():
     response.raise_for_status()
 
     url = "http://massive.ucsd.edu/ProteoSAFe/QueryPROXI?task=N%2FA&file=&pageSize=30&offset=0&query=%2523%257B%2522searched_button%2522%253A%2522mztabs%2522%257D&query_type=mztab&_=1489704532491"
-    response = requests.get(url, timeout=10)
+    response = requests.get(url, timeout=60)
     content = response.json()
     assert(content["source"] == "PROXI")
     assert(len(content["row_data"]) == 30)
@@ -61,7 +61,7 @@ def test_search_loadtime():
     response.raise_for_status()
 
     url = "http://massive.ucsd.edu/ProteoSAFe/QueryPROXI?task=N%2FA&file=&pageSize=30&offset=0&query=%2523%257B%2522searched_button%2522%253A%2522peptides%2522%252C%2522table_sort_history%2522%253A%2522variant_sequence_dsc%253Bsequence_dsc%2522%257D&query_type=peptide&_=1489704532502"
-    response = requests.get(url, timeout=20)
+    response = requests.get(url, timeout=60)
     content = response.json()
     assert(content["source"] == "PROXI")
     assert(len(content["row_data"]) == 30)
